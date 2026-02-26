8049bfa3146eb33530ca25aa8466d3a8581acdfe
Krunal <krunalsakpal679@gmail.com>
2026-02-26 21:36:07 +0530

    Initial commit - Timetable Project

--- timetable_project/timetable_project/scheduler/management/commands/create_sample_data.py
+++
@@ -1,8 +0,0 @@
-# scheduler/management/commands/create_sample_data.py
-from django.core.management.base import BaseCommand
-
-class Command(BaseCommand):
-    help = "Test command"
-
-    def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS("create_sample_data command is working!"))
--- timetable_project/timetable_project/scheduler/urls.py
+++
@@ -1,9 +0,0 @@
-# timetable_project/scheduler/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('regenerate/', views.regenerate_timetable, name='regenerate_timetable'),
-    path("download-pdf/", views.download_timetable_pdf, name="download_timetable_pdf"),
-]
